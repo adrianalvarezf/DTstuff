c3a2a343fa6830b0a039265c98d9d0cd82b4ec96
Adrian <Adrian.Alvarez@ciemat.es>
2017-04-25 15:55:29 +0200

    first commit

---
+++ cosmics_analysis/april2017/histodiv.C
@@ -0,0 +1,197 @@
+#if !defined(__CINT__) || defined(__MAKECINT__)
+
+#include <TROOT.h>
+#include <TH1.h>
+#include <TH2.h>
+#include <TSystem.h>
+#include <TStyle.h>
+#include <TCanvas.h>
+#include <TPad.h>
+#include <THStack.h>
+#include <TList.h>
+#include <TFrame.h>
+#include <TPostScript.h>
+#include <TPaveLabel.h>
+#include <TText.h>
+#include <TLatex.h>
+#include <TMath.h>
+#include <TF1.h>
+#include <TF2.h>
+#include <TFile.h>
+#include <TColor.h>
+#include <TString.h>
+
+#include <iostream>
+#include <fstream>
+
+#endif
+
+
+int main(int argc, const char* argv[]){
+
+  if(argc<3){printf("Not enough arguments provided. Use: ./histodiv.exe [numberoffiles N] [print gifs 1=y/0=n] file1.root ...  fileN.root\n"); return 0;}
+
+  TH1F *HH[5][4][14];  
+  TH1F *HL[5][4][14]; 
+  TH1F *HH_clone[5][4][14];  
+  TH1F *HL_clone[5][4][14]; 
+  TH1F *div[5][4][14];
+  TH1F *HH_fold[5][4][14];  
+  TH1F *HL_fold[5][4][14];
+  TH1F *HH_clone_fold[5][4][14];  
+  TH1F *HL_clone_fold[5][4][14];
+  TH1F *div_fold[5][4][14]; 
+  TF1  *myfit = new TF1("myfit","gaus(0)+pol0(3)",5,15);
+  int filenum= atoi(argv[1]);
+  int printgif= atoi(argv[2]);
+
+  for(int wheel=0;wheel<5;wheel++){
+    for(int station=0;station<4;station++){
+      for(int sector=0;sector<14;sector++){
+	TString hName = Form("Wh%d_St%d_Sec%d",wheel-2,station+1,sector+1);
+	TString hNamef = Form("Wh%d_St%d_Sec%d",wheel-2,station+1,sector+1);
+	
+	HL[wheel][station][sector] = new TH1F(Form("%s_HL",hName.Data()),hName,120,-80,40); 
+	HH[wheel][station][sector] = new TH1F(Form("%s_HH",hName.Data()),hName,120,-80,40); 
+	HL_fold[wheel][station][sector] = new TH1F(Form("%s_HL_fold",hNamef.Data()),hName,50,0,25); 
+	HH_fold[wheel][station][sector] = new TH1F(Form("%s_HH_fold",hNamef.Data()),hName,50,0,25); 
+	 
+	HL_clone[wheel][station][sector] = new TH1F(Form("%s_HL_clone",hName.Data()),hName,120,-80,40); 
+	HH_clone[wheel][station][sector] = new TH1F(Form("%s_HH_clone",hName.Data()),hName,120,-80,40); 
+	HL_clone_fold[wheel][station][sector] = new TH1F(Form("%s_HL_clone_fold",hNamef.Data()),hName,50,0,25); 
+	HH_clone_fold[wheel][station][sector] = new TH1F(Form("%s_HH_clone_fold",hNamef.Data()),hName,50,0,25); 
+
+	div[wheel][station][sector]= new TH1F(Form("%s_div",hName.Data()),hName,120,-80,40);
+	div_fold[wheel][station][sector]= new TH1F(Form("%s_div_fold",hNamef.Data()),hName,50,0,25); 
+      }
+    }
+  }
+
+  TFile *f[filenum];
+  //f[0] = new TFile("run291222_3600V_histograms.root","READ");
+  //f[0] = new TFile("run290966_3500V_histograms.root","READ");
+  //f[0] = new TFile("run290910_3400V_histograms.root","READ");
+  for(int i=0;i<filenum;i++) f[i] = new TFile(argv[i+3],"READ");
+
+  for (int nfile=0;nfile<filenum;nfile++){
+    for(int wheel=0;wheel<5;wheel++){
+      for(int station=0;station<4;station++){
+	for(int sec=0;sec<14;sec++){
+	  if(station==2||station==1||wheel==1||wheel==3)continue;                  ///Check just the changed chambers
+	  if(sec<2||sec>4)continue;                                                ///Check just the changed chambers
+	  HH[wheel][station][sec] = (TH1F*)f[nfile]->Get(Form("Wh%d_St%d_Sec%d_HH",wheel-2,station+1,sec+1));
+	  HL[wheel][station][sec] = (TH1F*)f[nfile]->Get(Form("Wh%d_St%d_Sec%d_HL",wheel-2,station+1,sec+1));
+	  HH_fold[wheel][station][sec] = (TH1F*)f[nfile]->Get(Form("Wh%d_St%d_Sec%d_HH_fold",wheel-2,station+1,sec+1));
+	  HL_fold[wheel][station][sec] = (TH1F*)f[nfile]->Get(Form("Wh%d_St%d_Sec%d_HL_fold",wheel-2,station+1,sec+1));
+	  if(nfile==0){
+	    HH_clone[wheel][station][sec]=(TH1F*)HH[wheel][station][sec]->Clone(); 
+	    HL_clone[wheel][station][sec]=(TH1F*)HL[wheel][station][sec]->Clone(); 
+	    HH_clone_fold[wheel][station][sec]=(TH1F*)HH_fold[wheel][station][sec]->Clone(); 
+	    HL_clone_fold[wheel][station][sec]=(TH1F*)HL_fold[wheel][station][sec]->Clone(); 
+	  }
+	  else{
+	    HH_clone[wheel][station][sec]->Add(HH[wheel][station][sec]);
+	    HL_clone[wheel][station][sec]->Add(HL[wheel][station][sec]);
+	    HH_clone_fold[wheel][station][sec]->Add(HH_fold[wheel][station][sec]);
+	    HL_clone_fold[wheel][station][sec]->Add(HL_fold[wheel][station][sec]);
+	  }
+	}
+      }
+    }
+  }
+   
+  TString sInput =argv[3];
+  TString sBlabla=sInput.ReplaceAll("_histograms.root","");
+  TString sOutput=Form("hist_div_%s",sBlabla.Data()); 
+  TFile *my_new_file = new TFile(Form("%s.root",sOutput.Data()),"RECREATE");
+
+  std::ofstream foldedfit (Form("%s.txt",sOutput.Data()));
+  foldedfit<<"    Wheel "<<"  "<<" MB "<<" "<<" Sector "<<"    "<<" mean "<<" "<<" mean error "<<" "<<" sigma "<<" sigma error "<<std::endl;
+  foldedfit<< " ................................................................................. " <<std::endl;
+
+ 
+  TCanvas *can[5][4][14][2];
+  for(int wheel=0;wheel<5;wheel++){
+    for(int station=0;station<4;station++){
+      for(int sec=0;sec<14;sec++){
+	if(HH_clone[wheel][station][sec]->GetEntries()==0 || HL_clone[wheel][station][sec]->GetEntries()==0)continue;
+	TString namediv = Form("HL_vs_HH_Wh%d_MB%d_Sec%d.gif",wheel-2,station+1,sec+1);
+	TString namedivf = Form("HL_vs_HH_folded_Wh%d_MB%d_Sec%d.gif",wheel-2,station+1,sec+1);
+	
+	can[wheel][station][sec][0]= new TCanvas(); 
+	can[wheel][station][sec][0]->cd(1);
+	div[wheel][station][sec]=(TH1F*)HL_clone[wheel][station][sec]->Clone();
+	div[wheel][station][sec]->Draw();    
+	div[wheel][station][sec]->Draw("E1SAME");   
+	for(int b=1;b<121;b++){
+	  double hh= HH_clone[wheel][station][sec]->GetBinContent(b);
+	  double hl= HL_clone[wheel][station][sec]->GetBinContent(b);
+	  double err= TMath::Sqrt((TMath::Sqrt(hl)/hh)*(TMath::Sqrt(hl)/hh)+(TMath::Sqrt(hh)*hl/(hh*hh))*(TMath::Sqrt(hh)*hl/(hh*hh)));
+	  div[wheel][station][sec]->SetBinError(b,err);
+	} 
+	div[wheel][station][sec]->Divide(HH_clone[wheel][station][sec]);
+	div[wheel][station][sec]->GetYaxis()->SetRangeUser(0,6);
+	div[wheel][station][sec]->GetXaxis()->SetTitle("t0(ns)");
+	div[wheel][station][sec]->GetYaxis()->SetTitle("HL/HH");
+	div[wheel][station][sec]->SetBins(120,-80,40);
+	div[wheel][station][sec]->SetLineColor(2);
+	div[wheel][station][sec]->SetTitle(namediv);
+	//can[wheel][station][sec][0]->Print(namediv); 
+ 	gStyle->SetOptStat(0);
+	can[wheel][station][sec][0]->Close();  
+
+	//////////////////////////////////////////////FOLDED HISTOS///////////////////////////////////////////////////////////////////
+	can[wheel][station][sec][1]= new TCanvas(); 
+	can[wheel][station][sec][1]->cd(1);
+	div_fold[wheel][station][sec]=(TH1F*)HL_clone_fold[wheel][station][sec]->Clone();
+	div_fold[wheel][station][sec]->SetName(Form("HLvsHH_Wh%d_MB%d_Sec%d",wheel-2,station+1,sec+1));
+	div_fold[wheel][station][sec]->Draw();    
+	div_fold[wheel][station][sec]->Draw("E1SAME");   
+	for(int bu=1;bu<51;bu++){
+	  double hh= HH_clone_fold[wheel][station][sec]->GetBinContent(bu);
+	  double hl= HL_clone_fold[wheel][station][sec]->GetBinContent(bu);
+	  double err= TMath::Sqrt((TMath::Sqrt(hl)/hh)*(TMath::Sqrt(hl)/hh)+(TMath::Sqrt(hh)*hl/(hh*hh))*(TMath::Sqrt(hh)*hl/(hh*hh)));
+	  div_fold[wheel][station][sec]->SetBinError(bu,err);
+	}
+	div_fold[wheel][station][sec]->Divide(HH_clone_fold[wheel][station][sec]);
+	//div_fold[wheel][station][sec]->GetYaxis()->SetRangeUser(0,2.5);
+	div_fold[wheel][station][sec]->GetXaxis()->SetTitle("t0_folded(ns)");
+	div_fold[wheel][station][sec]->GetYaxis()->SetTitle("HL/HH");
+	div_fold[wheel][station][sec]->SetBins(50,0,25);
+	div_fold[wheel][station][sec]->SetLineColor(2);
+	 
+	int  binmax = div_fold[wheel][station][sec]->GetMaximumBin();
+	double xmax = div_fold[wheel][station][sec]->GetXaxis()->GetBinCenter(binmax);
+	double ymax = div_fold[wheel][station][sec]->GetMaximum();
+	double ymin = div_fold[wheel][station][sec]->GetMinimum();
+	myfit->SetParameter(0,ymax-ymin);
+	myfit->SetParameter(1,xmax);
+	myfit->SetParameter(2,3);
+	//if(wheel==0&&station==0&&sec==8)myfit->SetParameter(1,5);
+	//if(wheel==1&&station==0&&sec==0)myfit->SetParameter(1,9);
+	myfit->SetParameter(3,ymin);
+	div_fold[wheel][station][sec]->Fit("myfit","","",0,25);
+	double finalmean = myfit->GetParameter(1);
+	double finalsigma = myfit->GetParameter(2);
+	double finalmeanerr = myfit->GetParError(1);
+	double finalsigmaerr = myfit->GetParError(2);
+	div_fold[wheel][station][sec]->SetTitle(namedivf);
+	myfit->SetParName(0,"peak_height");
+	myfit->SetParName(1,"peak_center");
+	myfit->SetParName(2,"peak_sigma");
+	myfit->SetParName(3,"initial_height");
+	gStyle->SetOptFit(0111);
+	gStyle->SetOptStat(0);
+	TString namechamber = Form("Wh%d_MB%d_Sec%d",wheel-2,station+1,sec+1);
+	foldedfit<<"      "<<std::showpos<<wheel-2<<std::noshowpos<<"      "<<station+1<<"      "<<std::setfill('0') << std::setw(2)<< sec+1<<"      "<< std::fixed << std::setprecision(4)<<std::setw(7)<<finalmean<<std::setw(2)<<"   "<<finalmeanerr<<"     "<<fabs(finalsigma)<<"   "<<finalsigmaerr<<std::endl;		
+	div_fold[wheel][station][sec]->Write();
+	if(printgif==1)can[wheel][station][sec][1]->Print(namedivf);  
+	can[wheel][station][sec][1]->Close();  
+	
+      } 
+    }
+  }
+  
+  return 0;
+
+}
---
+++ cosmics_analysis/april2017/histograms_HH_and_HL.C
@@ -0,0 +1,205 @@
+
+#define histograms_HH_and_HL_cxx
+
+#include "histograms_HH_and_HL.h"
+#include <TH2.h>
+#include <TStyle.h>
+#include <stdio.h>
+#include <string>
+#include "Riostream.h"
+#include "TVectorT.h"
+#include "TString.h"
+
+void histograms_HH_and_HL::Begin(TTree * /*tree*/)
+{
+ 
+  TString option = GetOption();
+
+}
+
+void histograms_HH_and_HL::SlaveBegin(TTree * /*tree*/)
+{
+  TString option = GetOption();
+}
+
+Bool_t histograms_HH_and_HL::Process(Long64_t entry)
+{
+  vector<short>   &my_dtsegm4D_wheel= *dtsegm4D_wheel;
+  vector<short>   &my_dtsegm4D_station= *dtsegm4D_station; 
+  vector<short>   &my_dtsegm4D_sector= *dtsegm4D_sector;
+  vector<short>   &my_dtsegm4D_phinhits= *dtsegm4D_phinhits;
+  vector<float>   &my_dtsegm4D_t0 = *dtsegm4D_t0;
+  vector<short>   &my_ltTwinMuxIn_quality= *ltTwinMuxIn_quality;
+  vector<short>   &my_ltTwinMuxIn_wheel= *ltTwinMuxIn_wheel;
+  vector<short>   &my_ltTwinMuxIn_sector= *ltTwinMuxIn_sector;
+  vector<short>   &my_ltTwinMuxIn_station= *ltTwinMuxIn_station;
+  vector<float>   &my_dtsegm4D_x_dir_loc = *dtsegm4D_x_dir_loc;
+  vector<float>   &my_dtsegm4D_z_dir_loc = *dtsegm4D_z_dir_loc;
+
+  vector<short>   &my_Mu_isMuGlobal = *Mu_isMuGlobal;
+  vector<short>   &my_Mu_isMuTracker = *Mu_isMuTracker;
+  vector<float>   &my_Mu_normchi2_glb = *Mu_normchi2_glb;
+  vector<int>     &my_Mu_numberOfMatches_sta = *Mu_numberOfMatches_sta;
+  vector<float>   &my_Mu_dz_glb = *Mu_dz_glb;
+  vector<float>   &my_Mu_dxy_glb = *Mu_dxy_glb;
+  vector<int>     &my_Mu_numberOfPixelHits_glb = *Mu_numberOfPixelHits_glb;
+  vector<int>     &my_Mu_numberOfTrackerHits_glb = *Mu_numberOfTrackerHits_glb;
+  vector<int>     &my_Mu_numberOfHits_sta = *Mu_numberOfHits_sta;
+
+
+  fChain->GetEntry(entry);
+  if(fmod(entry,10000)==0) Printf(" ..... event %d", int(entry));
+
+
+  int ndtsegm4D =  dtsegm4D_station->size();
+  totalsegm+=ndtsegm4D;
+
+  for (int idtsegm = 0; idtsegm < ndtsegm4D; ++idtsegm) {
+    if(my_ltTwinMuxIn_quality.size()==0){continue;}
+    TVectorF *my_segm4D_phi_hitsLayer = (TVectorF*)dtsegm4D_phi_hitsLayer->At(idtsegm);
+    int n_phi_hitsLayer = my_segm4D_phi_hitsLayer->GetNoElements();
+    float ang_rec=TMath::ATan(my_dtsegm4D_x_dir_loc[idtsegm]/my_dtsegm4D_z_dir_loc[idtsegm]);
+    if(fabs(ang_rec) >TMath::Pi()/6)continue;
+    //if(n_phi_hitsLayer<6){continue;}
+    
+    for (int q = 0; q <(int)my_ltTwinMuxIn_quality.size(); ++q) {
+      if(my_ltTwinMuxIn_station[q]!= my_dtsegm4D_station[idtsegm] || my_ltTwinMuxIn_wheel[q]!= my_dtsegm4D_wheel[idtsegm]){continue;}
+      if(my_ltTwinMuxIn_sector[q]!= my_dtsegm4D_sector[idtsegm] &&  my_dtsegm4D_sector[idtsegm]<13){continue;}
+      if(my_dtsegm4D_sector[idtsegm]==13 && my_ltTwinMuxIn_sector[q]!=4){continue;}
+      if(my_dtsegm4D_sector[idtsegm]==14 && my_ltTwinMuxIn_sector[q]!=10){continue;}
+      // T0[my_ltTwinMuxIn_wheel[q]+2][my_ltTwinMuxIn_station[q]-1][my_ltTwinMuxIn_sector[q]-1]->Fill(my_dtsegm4D_t0[idtsegm]);
+      if(my_ltTwinMuxIn_quality[q]==6){
+	HH[my_ltTwinMuxIn_wheel[q]+2][my_ltTwinMuxIn_station[q]-1][my_dtsegm4D_sector[idtsegm]-1]->Fill(my_dtsegm4D_t0[idtsegm]);
+	HH_fold[my_ltTwinMuxIn_wheel[q]+2][my_ltTwinMuxIn_station[q]-1][my_dtsegm4D_sector[idtsegm]-1]->Fill((int)(my_dtsegm4D_t0[idtsegm]+50)%25);
+	highhigh++;both++;
+	break;
+      }
+      if(my_ltTwinMuxIn_quality[q]==5){
+	HL[my_ltTwinMuxIn_wheel[q]+2][my_ltTwinMuxIn_station[q]-1][my_dtsegm4D_sector[idtsegm]-1]->Fill(my_dtsegm4D_t0[idtsegm]);
+	HL_fold[my_ltTwinMuxIn_wheel[q]+2][my_ltTwinMuxIn_station[q]-1][my_dtsegm4D_sector[idtsegm]-1]->Fill((int)(my_dtsegm4D_t0[idtsegm]+50)%25);
+	highlow++;both++;
+	break;
+      }
+      if(my_ltTwinMuxIn_quality[q]<5){worseq++;	break;}
+    }
+  }
+ 
+ 
+ return kTRUE;
+
+ my_dtsegm4D_wheel.clear();
+ my_dtsegm4D_station.clear(); 
+ my_dtsegm4D_sector.clear();
+ my_dtsegm4D_phinhits.clear();
+ my_dtsegm4D_t0.clear();
+ my_ltTwinMuxIn_quality.clear();
+ my_ltTwinMuxIn_sector.clear();
+ my_ltTwinMuxIn_station.clear();
+ my_ltTwinMuxIn_wheel.clear();
+ my_dtsegm4D_x_dir_loc.clear();
+ my_dtsegm4D_z_dir_loc.clear();
+
+ my_Mu_isMuGlobal.clear();
+ my_Mu_isMuTracker.clear();
+ my_Mu_normchi2_glb.clear();
+ my_Mu_numberOfMatches_sta.clear();
+ my_Mu_dxy_glb.clear();
+ my_Mu_dz_glb.clear();
+ my_Mu_numberOfHits_sta.clear();
+ my_Mu_numberOfPixelHits_glb.clear();
+ my_Mu_numberOfTrackerHits_glb.clear();
+
+}
+ 
+void histograms_HH_and_HL::SlaveTerminate()
+{
+    
+}
+
+void histograms_HH_and_HL::Terminate()
+{
+
+  cout<<" Number of HH = "<<highhigh<<"," <<" Number of HL = "<<highlow<<"," <<" Number of both = "<<both<<","<<" Number of bad quality segments "<<worseq<<","<<" Number of segments "<<totalsegm<<endl;
+ 
+  int voltage;
+  if(runnumber==291316)voltage=3700;
+  if(runnumber==291222)voltage=3600;
+  if(runnumber==290966)voltage=3500;
+  if(runnumber==290910)voltage=3400;
+  if(runnumber==291340)voltage=3250;
+  if(runnumber==291385)voltage=3350;
+
+
+  TFile *my_new_file = new TFile(Form("run%d_%dV_histograms.root",runnumber,voltage),"RECREATE"); 
+  TCanvas *can2[5][4][14][2];
+  for(int wheel=0;wheel<5;wheel++){
+    for(int station=0;station<4;station++){
+      for(int sec=0;sec<14;sec++){
+	for(int qu=0;qu<2;qu++){
+	  TString name[2]={Form("HL_Wh%d_MB%d_Sec%d.gif",wheel-2,station+1,sec+1),Form("HH_Wh%d_MB%d_Sec%d.gif",wheel-2,station+1,sec+1)};
+	  if(sec>11&&station!=3)continue;
+	  can2[wheel][station][sec][qu]= new TCanvas(); 
+	  can2[wheel][station][sec][qu]->cd(1);
+	  if(qu==1){
+	    HH[wheel][station][sec]->Draw();
+	    HH[wheel][station][sec]->Draw("E1SAME");
+	    HH[wheel][station][sec]->SetBins(120,-80,40);
+	    HH[wheel][station][sec]->SetTitle(name[qu]);
+	    HH[wheel][station][sec]->GetXaxis()->SetTitle("t0(ns)");
+	    HH[wheel][station][sec]->GetYaxis()->SetTitle("entries");
+	    HH[wheel][station][sec]->Write();
+	  }
+	  if(qu==0){
+	    HL[wheel][station][sec]->Draw();
+	    HL[wheel][station][sec]->Draw("E1SAME");
+	    HL[wheel][station][sec]->SetBins(120,-80,40);
+	    HL[wheel][station][sec]->SetTitle(name[qu]);
+	    HL[wheel][station][sec]->GetXaxis()->SetTitle("t0(ns)");
+	    HL[wheel][station][sec]->GetYaxis()->SetTitle("entries");
+	    HL[wheel][station][sec]->Write();
+	  }
+	  //can2[wheel][station][sec][qu]->Print(name[qu]);  
+	  can2[wheel][station][sec][qu]->Close();  
+	} 
+      }
+    }
+  }
+ TCanvas *can22[5][4][14][2];
+  for(int wheel=0;wheel<5;wheel++){
+    for(int station=0;station<4;station++){
+      for(int sec=0;sec<14;sec++){
+	for(int qu=0;qu<2;qu++){
+	  TString name[2]={Form("HL_fold_Wh%d_MB%d_Sec%d.gif",wheel-2,station+1,sec+1),Form("HH_fold_Wh%d_MB%d_Sec%d.gif",wheel-2,station+1,sec+1)};
+	  if(sec>11&&station!=3)continue;
+	  can22[wheel][station][sec][qu]= new TCanvas(); 
+	  can22[wheel][station][sec][qu]->cd(1);
+	  if(qu==1){
+	    HH_fold[wheel][station][sec]->Draw();
+	    HH_fold[wheel][station][sec]->Draw("E1SAME");
+	    HH_fold[wheel][station][sec]->SetBins(50,0,25);
+	    HH_fold[wheel][station][sec]->SetTitle(name[qu]);
+	    HH_fold[wheel][station][sec]->GetXaxis()->SetTitle("t0(ns)");
+	    HH_fold[wheel][station][sec]->GetYaxis()->SetTitle("entries");
+	    HH_fold[wheel][station][sec]->Write();	 
+	  }
+	  if(qu==0){
+	    HL_fold[wheel][station][sec]->Draw();
+	    HL_fold[wheel][station][sec]->Draw("E1SAME");
+	    HL_fold[wheel][station][sec]->SetBins(50,0,25);
+	    HL_fold[wheel][station][sec]->SetTitle(name[qu]);
+	    HL_fold[wheel][station][sec]->GetXaxis()->SetTitle("t0(ns)");
+	    HL_fold[wheel][station][sec]->GetYaxis()->SetTitle("entries");
+	    HL_fold[wheel][station][sec]->Write();
+	  }
+	  //can22[wheel][station][sec][qu]->Print(name[qu]);  
+	  can22[wheel][station][sec][qu]->Close();  
+	} 
+      }
+    }
+  }
+  
+
+  
+
+  
+}
